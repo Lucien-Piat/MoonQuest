12eb9a1a2a58b613af9968d698f9875d103c7a41
Lucien-Piat <lucienpiat33@gmail.com>
2024-04-02 13:30:15 +0200

    Creating nuage movement patern

--- src/objets/pieces/abstract_class/Nuage.java
+++ src/objets/pieces/abstract_class/Nuage.java
@@ -1,9 +1,21 @@
 package objets.pieces.abstract_class;
 
+import java.util.Random;
+
 public class Nuage extends Piece {
+
+    final String[] directions = {"nord", "nord_est", "est", "sud_est", "sud", "sud_ouest", "ouest", "nord_ouest"};
 
     public Nuage(String toPrint){
         super(toPrint); 
     }
-    
+
+    public String move(){
+        Random random = new Random();
+        if (random.nextInt(5) == 0) { // 0 to 4 (inclusive)
+            Random random2 = new Random();
+            int randomIndex = random2.nextInt(directions.length);
+            return directions[randomIndex];
+        }else return "bouge_pas";
+    } 
 }
