013b821d4698e4760036e83f99f28c9f27c6ab9c
Lucien-Piat <lucienpiat33@gmail.com>
2024-04-02 19:10:32 +0200

    test commit on second pc

--- src/App.java
+++ src/App.java
@@ -4,8 +4,8 @@ import objets.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Plateau board = new Plateau();
-        System.out.println(board.getCases()[26].getName());
-        System.out.println(board.getCases()[26].findDestinationCase("nord", board.getBoardSize(),2));
+        System.out.println(board.getCases()[27].getName());
+        System.out.println(board.getCases()[27].findDestinationCase("nord", board.getBoardSize(),2));
         board.display();
 
     }
